ac42244888aa026be0b65dae77a04efe9438a8c3
Roman Fedoriv <fedoriv.roman.v@gmail.com>
2025-09-18 14:07:02 +0300

    setup

---
+++ src/services/noteService.ts
@@ -0,0 +1,13 @@
+
+
+export default function fetchNotes () {
+    // Логіка отримання нотаток
+}
+
+export default function createNote (note) {
+    // Логіка створення нотатки
+}
+
+export default function deleteNote (id, notes) {
+// Логіка видалення нотатки
+} 
